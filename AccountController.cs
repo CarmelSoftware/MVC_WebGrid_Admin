db5b3a850bb198fffc101cce9de9f57e3abea6d1
Carmel Schvartzman <CarmelSoftware@users.noreply.github.com>
2015-10-25 14:33:07 +0200

    Create AccountController.cs

---
+++ AccountController.cs
@@ -0,0 +1,49 @@
+// THIS IS THE MODIFIED "REGISTER" ACTION
+//    INSIDE THE "ACCOUNT" CONTROLLER:
+
+[HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Attempt to register the user
+                try
+                {
+                    if (!Roles.RoleExists("Admin"))
+                    {
+                        Roles.CreateRole("Admin");
+                    }
+                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
+
+//  GET THE ADMINISTRATORS:
+                    string sAdmins = ConfigurationManager.AppSettings["Admins"];
+                    string[] oAdmins = sAdmins.Split(';');
+
+                    WebSecurity.Login(model.UserName, model.Password);
+
+//  ADD THE CURRENT USER TO THE "ADMIN" ROLE:
+                    foreach (string sAdmin in oAdmins)
+                    {
+                        if (string.Compare(model.UserName, sAdmin) == 0
+                                                &&
+                            !Roles.IsUserInRole("Admin"))
+                        {
+                            Roles.AddUserToRole(model.UserName, "Admin");
+                        }
+                    }
+
+
+
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
